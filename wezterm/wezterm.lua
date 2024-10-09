2fa59ff7684898f5569370b152a2be4eefdc65a9
kevinxft <kevinxft@gmail.com>
2024-10-09 15:24:01 +0800

    remove kitty config folder

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,16 +2,25 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.default_cursor_style = 'BlinkingUnderline'
+
 config.color_scheme = 'Dark Ocean (terminal.sexy)'
+-- config.color_scheme = 'Dark Pastel'
 
 -- fonts
-config.font = wezterm.font 'MonacoLigaturized Nerd Font Mono'
-config.font_size = 14.0
-config.line_height = 1.2
+config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
+config.font_size = 16.0
+config.line_height = 1.1
+
+config.enable_tab_bar = false
+
+config.window_decorations = 'RESIZE'
+
+
 
 -- background
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 40
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 30
 
 
 return config
