07f988da2df0732d2e7bae7f17ace1615af2dbc1
kevinxft <kevinxft@gmail.com>
2024-10-23 13:18:24 +0800

    chore: :hammer: 优化切换到文件树

--- nvim/lua/config/core/keymaps.lua
+++ nvim/lua/config/core/keymaps.lua
@@ -21,3 +21,33 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- save file
+keymap.set("n", "<Leader>w", ":w<CR>", { desc = "save file", noremap = true, silent = true })
+
+-- 定义一个函数来切换焦点
+local function toggle_tree_focus()
+  local nvim_tree = require("nvim-tree.view")
+  local current_winnr = vim.fn.winnr()
+
+  -- 如果 nvim-tree 没有打开，则打开它
+  if not nvim_tree.is_visible() then
+    vim.cmd("NvimTreeOpen")
+    vim.cmd("NvimTreeFocus")
+    return
+  end
+
+  -- 获取 nvim-tree 窗口编号
+  local tree_winnr = vim.fn.bufwinnr("NvimTree_1")
+
+  -- 如果当前在 nvim-tree 窗口，切换到上一个窗口
+  if current_winnr == tree_winnr then
+    vim.cmd("wincmd p")
+  else
+    -- 否则，切换到 nvim-tree 窗口
+    vim.cmd(string.format("%dwincmd w", tree_winnr))
+  end
+end
+
+-- 映射 Ctrl+; 快捷键
+vim.keymap.set("n", "<C-;>", toggle_tree_focus, { noremap = true, silent = true })
